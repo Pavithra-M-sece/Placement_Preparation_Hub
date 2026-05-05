9b522d4744e2939cd59f555efde4f20a2202251a
PAVITHRA M <pavithra.m2024cse@sece.ac.in>
2026-05-05 15:49:06 +0530

    Delete start-backend.bat

--- start-backend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.18.8-hotspot
-set PATH=%JAVA_HOME%\bin;C:\maven\apache-maven-3.9.6\bin;%PATH%
-cd /d "d:\preparation hub\backend"
-echo Starting Spring Boot...
-mvn spring-boot:run
-pause
